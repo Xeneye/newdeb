1669c792cae18f16859dfac17a6d8a391cd007e5
Paul Fowler <43384818+Xeneye@users.noreply.github.com>
2019-06-01 03:30:49 -0400

    Create 10-tweaks.sh

---
+++ 10-tweaks.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+wget https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh -O- | sh
+wget -qO- https://raw.githubusercontent.com/protesilaos/prot16-xfce4-terminal/master/install-prot16-xfce-terminal.sh | sh
