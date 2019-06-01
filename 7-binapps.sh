5c9eee9f41e81ea282bfc6c31d02766081294510
Paul Fowler <43384818+Xeneye@users.noreply.github.com>
2019-06-01 03:17:57 -0400

    Update 7-binapps.sh

--- 7-binapps.sh
+++ 7-binapps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt install htop neofetch cmatrix cmatrix-xfont lolcat cowsay-off fortunes fortune-mod autokey-gtk catfish grub-customizer gdebi
+sudo apt install htop neofetch cmatrix cmatrix-xfont lolcat cowsay-off fortunes fortune-mod autokey-gtk catfish grub-customizer gdebi rsync gnome-disk-utility
