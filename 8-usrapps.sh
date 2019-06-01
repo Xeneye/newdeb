277f9878de6ac25757afc545d0f8131bb28585fb
Paul Fowler <43384818+Xeneye@users.noreply.github.com>
2019-06-01 03:17:26 -0400

    Create 8-usrapps.sh

---
+++ 8-usrapps.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt install firefox-esr firefox-esr-l10n-en-gb thunderbird thunderbird-l10n-en-gb lightning-l10n-en-gb hunspell hunspell-en-gb hyphen hyphen-en-gb telegram-desktop soundconverter audacity 
