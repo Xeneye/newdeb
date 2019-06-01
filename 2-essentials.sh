5aef96113a23647156f15fffbdb29199bc7f85e6
Paul Fowler <43384818+Xeneye@users.noreply.github.com>
2019-06-01 03:23:43 -0400

    Update 2-essentials.sh

--- 2-essentials.sh
+++ 2-essentials.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt install aptitude synaptic git wget locate nfs-common cups cups-common locales-all apt-xapian-index network-manager network-manager-config-connectivity-debian autoconf automake libtool unrar p7zip-full p7zip-rar
+sudo apt install aptitude synaptic git wget locate nfs-common cups cups-common locales-all apt-xapian-index network-manager network-manager-config-connectivity-debian autoconf automake libtool unrar p7zip-full p7zip-rar cmake libudev-dev qt5-default zlib1g-dev libappindicator-dev libpulse-dev libquazip5-dev
