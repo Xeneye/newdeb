c3d642b906467e6f1f0fae28a47d9a13140498c1
Paul Fowler <43384818+Xeneye@users.noreply.github.com>
2019-06-14 21:45:01 +0100

    Update 01-allinone-1.sh

--- 01-allinone-1.sh
+++ 01-allinone-1.sh
@@ -18,7 +18,8 @@ clear
 echo "---------------------"
 echo "Installing essentials"
 echo "---------------------"
-sudo apt install -y aptitude synaptic git wget locate nfs-common cups cups-common locales-all apt-xapian-index network-manager network-manager-config-connectivity-debian autoconf automake libtool unrar p7zip-full p7zip-rar cmake libudev-dev qt5-default zlib1g-dev libappindicator-dev libpulse-dev libquazip5-dev meson libsystemd-dev pkg-config ninja-build net-tools shellcheck checkinstall devscripts build-essential lintian 
+sudo apt install -y aptitude synaptic git wget locate nfs-common cups cups-common locales-all apt-xapian-index network-manager network-manager-config-connectivity-debian autoconf automake libtool unrar p7zip-full p7zip-rar cmake libudev-dev qt5-default zlib1g-dev libappindicator-dev libpulse-dev libquazip5-dev meson libsystemd-dev pkg-config ninja-build net-tools shellcheck checkinstall devscripts build-essential lintian nvidia-cuda-toolkit
+
 clear
 
 echo "----------------------------------"
