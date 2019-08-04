6f184047c541af7d16d8761b8124f56fb9c8d72b
Paul Fowler <43384818+Xeneye@users.noreply.github.com>
2019-08-04 15:24:27 +0100

    Update 00-update&reboot.sh

--- 00-update&reboot.sh
+++ 00-update&reboot.sh
@@ -4,31 +4,7 @@ sudo dpkg --add-architecture i386
 
 ## sources tweaks
 echo "Adding sources tweaks"	
-echo "########################################################################################################################                                                                                                                                                                                                                 
-#XXXXXXX       XXXXXXX                                                                                                  #
-#X:::::X       X:::::X                                                                                                  #
-#X:::::X       X:::::X                                                                                                  #
-#X::::::X     X::::::X                                                                                                  #
-#XXX:::::X   X:::::XXX    eeeeeeeeeeee    nnnn  nnnnnnnn        eeeeeeeeeeee  yyyyyyy           yyyyyyy eeeeeeeeeeee    #
-#   X:::::X X:::::X     ee::::::::::::ee  n:::nn::::::::nn    ee::::::::::::ee y:::::y         y:::::yee::::::::::::ee  #
-#    X:::::X:::::X     e::::::eeeee:::::een::::::::::::::nn  e::::::eeeee:::::eey:::::y       y:::::ye::::::eeeee:::::ee#
-#     X:::::::::X     e::::::e     e:::::enn:::::::::::::::ne::::::e     e:::::e y:::::y     y:::::ye::::::e     e:::::e#
-#     X:::::::::X     e:::::::eeeee::::::e  n:::::nnnn:::::ne:::::::eeeee::::::e  y:::::y   y:::::y e:::::::eeeee::::::e#
-#    X:::::X:::::X    e:::::::::::::::::e   n::::n    n::::ne:::::::::::::::::e    y:::::y y:::::y  e:::::::::::::::::e #
-#   X:::::X X:::::X   e::::::eeeeeeeeeee    n::::n    n::::ne::::::eeeeeeeeeee      y:::::y:::::y   e::::::eeeeeeeeeee  #
-#XXX:::::X   X:::::XXXe:::::::e             n::::n    n::::ne:::::::e                y:::::::::y    e:::::::e           #
-#X::::::X     X::::::Xe::::::::e            n::::n    n::::ne::::::::e                y:::::::y     e::::::::e          #
-#X:::::X       X:::::X e::::::::eeeeeeee    n::::n    n::::n e::::::::eeeeeeee         y:::::y       e::::::::eeeeeeee  #
-#X:::::X       X:::::X  ee:::::::::::::e    n::::n    n::::n  ee:::::::::::::e        y:::::y         ee:::::::::::::e  #
-#XXXXXXX       XXXXXXX    eeeeeeeeeeeeee    nnnnnn    nnnnnn    eeeeeeeeeeeeee       y:::::y            eeeeeeeeeeeeee  #
-#                                                                                   y:::::y                             #
-#                                                                                  y:::::y                              #
-#                                                                                 y:::::y                               #
-#                                                                                y:::::y                                #
-#                                                                               yyyyyyy                                 #
-#########################################################################################################################                                                                                                                       
-                                                                                                                     
-deb http://deb.debian.org/debian/ buster main non-free contrib
+echo "deb http://deb.debian.org/debian/ buster main non-free contrib
 deb-src http://deb.debian.org/debian/ buster main non-free contrib
 
 deb http://security.debian.org/debian-security buster/updates main non-free contrib
