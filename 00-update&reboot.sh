a49dd30c7f645aa1d1a73a15467ee6a93a362bd3
Paul Fowler <43384818+Xeneye@users.noreply.github.com>
2019-08-04 14:41:30 +0100

    Update 00-update&reboot.sh

--- 00-update&reboot.sh
+++ 00-update&reboot.sh
@@ -2,9 +2,9 @@
 
 sudo dpkg --add-architecture i386
 
-sudo apt install -y apt-listbugs apt-listchanges
-
-sudo apt update -y && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y
+#sudo apt install -y apt-listbugs apt-listchanges
+#sudo apt update -y && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y
+sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y
 clear
 
 echo "-----------------"
