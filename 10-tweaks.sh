238bb46950b15def9cf429adb50db91159d157ca
Paul Fowler <43384818+Xeneye@users.noreply.github.com>
2019-08-18 21:01:17 +0100

    Update 10-tweaks.sh

--- 10-tweaks.sh
+++ 10-tweaks.sh
@@ -9,3 +9,5 @@ sleep 2
 wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-filezilla-themes/master/install.sh | sh
 sleep 2
 sudo apt -y remove xfburn xterm && sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y && sudo apt -y install hddtemp libburn4 libisofs6 libjte1 libqrencode4 libunique-1.0-0 libwnck-3-0 libwnck-3-common mousepad ristretto xfce4-battery-plugin xfce4-clipman xfce4-clipman-plugin xfce4-cpufreq-plugin xfce4-cpugraph-plugin xfce4-datetime-plugin xfce4-dict xfce4-diskperf-plugin xfce4-fsguard-plugin xfce4-genmon-plugin xfce4-mailwatch-plugin xfce4-netload-plugin xfce4-notes xfce4-notes-plugin xfce4-places-plugin xfce4-screenshooter xfce4-sensors-plugin xfce4-smartbookmark-plugin xfce4-systemload-plugin xfce4-taskmanager xfce4-timer-plugin xfce4-verve-plugin xfce4-wavelan-plugin xfce4-weather-plugin xfce4-whiskermenu-plugin xfce4-xkb-plugin
+sleep 2
+sudo chmod u+s /usr/sbin/hddtemp
