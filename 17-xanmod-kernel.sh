7fd9163df2b9a360a3032e892ecf3615bf2771c1
Paul Fowler <43384818+Xeneye@users.noreply.github.com>
2019-08-18 21:06:44 +0100

    Create 17-xanmod-kernel.sh

---
+++ 17-xanmod-kernel.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo 'deb http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-kernel.list && wget -qO - https://dl.xanmod.org/gpg.key | sudo apt-key add -
+sudo apt update && sudo apt install linux-xanmod
