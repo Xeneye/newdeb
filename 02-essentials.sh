b7a76565f40cf5e90d1eb1fab8849e90b894c50e
Paul Fowler <43384818+Xeneye@users.noreply.github.com>
2019-08-20 22:40:29 +0100

    Update 02-essentials.sh

--- 02-essentials.sh
+++ 02-essentials.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install aptitude synaptic git wget locate nfs-common cups cups-common locales-all apt-xapian-index network-manager network-manager-config-connectivity-debian autoconf automake libtool unrar p7zip-full p7zip-rar cmake libudev-dev qt5-default zlib1g-dev libappindicator-dev libpulse-dev libquazip5-dev meson libsystemd-dev pkg-config ninja-build net-tools shellcheck devscripts build-essential lintian libjs-bootstrap
+sudo apt install aptitude synaptic git wget locate nfs-common cups cups-common locales-all apt-xapian-index network-manager network-manager-config-connectivity-debian autoconf automake libtool unrar p7zip-full p7zip-rar cmake libudev-dev qt5-default zlib1g-dev libappindicator-dev libpulse-dev libquazip5-dev meson libsystemd-dev pkg-config ninja-build net-tools shellcheck devscripts build-essential lintian libjs-bootstrap amd64-microcode
 
 sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y
 clear
