88cd6683a3866ecd104ca92a60f2595e371bab48
Paul Fowler <43384818+Xeneye@users.noreply.github.com>
2019-08-20 21:48:02 +0100

    Update 04-fonts.sh

--- 04-fonts.sh
+++ 04-fonts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt install fonts-cantarell fonts-hack fonts-hack-otf fonts-hack-ttf fonts-hack-web ttf-mscorefonts-installer fonts-noto fonts-powerline fonts-font-awesome fonts-lmodern
+sudo apt install fonts-cantarell fonts-hack fonts-hack-otf fonts-hack-ttf fonts-hack-web ttf-mscorefonts-installer fonts-noto fonts-powerline fonts-font-awesome fonts-lmodern ttf-bitstream-vera ttf-dejavu ttf-dejavu-core ttf-dejavu-extra ttf-ubuntu-font-family 
