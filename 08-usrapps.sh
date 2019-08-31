69b320d3801e62ec9479108728aef8fe185b480c
Paul Fowler <43384818+Xeneye@users.noreply.github.com>
2019-08-31 16:48:07 +0100

    Update 08-usrapps.sh

--- 08-usrapps.sh
+++ 08-usrapps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt install firefox-esr firefox-esr-l10n-en-gb thunderbird thunderbird-l10n-en-gb lightning-l10n-en-gb hunspell hunspell-en-gb hyphen-en-gb telegram-desktop soundconverter audacity quodlibet obs-studio gimp inkscape blender handbrake steam filezilla
+sudo apt install thunderbird thunderbird-l10n-en-gb lightning-l10n-en-gb hunspell hunspell-en-gb hyphen-en-gb telegram-desktop soundconverter audacity quodlibet obs-studio gimp steam filezilla
