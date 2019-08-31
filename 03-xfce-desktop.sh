fd1a53804f54ba4cfa4ed29940c749c8a2759a4c
Paul Fowler <43384818+Xeneye@users.noreply.github.com>
2019-08-31 16:49:11 +0100

    Update 03-xfce-desktop.sh
    Removed Guake

--- 03-xfce-desktop.sh
+++ 03-xfce-desktop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt install xfce4 xfce4-goodies xfce4-indicator-plugin lightdm-gtk-greeter-settings clipit alsa-utils smbclient gvfs-bin gvfs-backends gstreamer1.0-tools gstreamer1.0-plugins-bad guake
+sudo apt install xfce4 xfce4-goodies xfce4-indicator-plugin lightdm-gtk-greeter-settings clipit alsa-utils smbclient gvfs-bin gvfs-backends gstreamer1.0-tools gstreamer1.0-plugins-bad
