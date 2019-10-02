efc686a60f599063be7608a1a749c08413bee80e
Paul Fowler <43384818+Xeneye@users.noreply.github.com>
2019-10-02 12:22:37 +0100

    Update 08-usrapps.sh
    Removed Thunderbird

--- 08-usrapps.sh
+++ 08-usrapps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt install thunderbird thunderbird-l10n-en-gb lightning-l10n-en-gb hunspell hunspell-en-gb hyphen-en-gb telegram-desktop soundconverter audacity quodlibet obs-studio gimp steam filezilla
+sudo apt install hunspell hunspell-en-gb hyphen-en-gb telegram-desktop soundconverter audacity quodlibet obs-studio gimp steam filezilla
