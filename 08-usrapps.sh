ba0b5bedf8419d0ebc8d4d1f0f90624c77427f8d
Paul Fowler <43384818+Xeneye@users.noreply.github.com>
2019-10-08 14:22:51 +0100

    Update 08-usrapps.sh

--- 08-usrapps.sh
+++ 08-usrapps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt install hunspell hunspell-en-gb hyphen-en-gb telegram-desktop soundconverter audacity quodlibet obs-studio gimp steam filezilla
+sudo apt install hunspell hunspell-en-gb hyphen-en-gb telegram-desktop soundconverter audacity quodlibet gimp steam filezilla
