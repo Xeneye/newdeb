0d69d6a7e2fa6001d3abcafe534df1d1b4728177
Paul Fowler <43384818+Xeneye@users.noreply.github.com>
2020-01-26 18:42:51 +0000

    Added extra build packages

--- 02-essentials.sh
+++ 02-essentials.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install aptitude synaptic git curl wget locate nfs-common cups cups-common locales-all apt-xapian-index network-manager network-manager-config-connectivity-debian autoconf automake libtool unrar p7zip-full p7zip-rar cmake libudev-dev qt5-default zlib1g-dev libappindicator-dev libpulse-dev libquazip5-dev meson libsystemd-dev pkg-config libdbus-1-dev ninja-build net-tools shellcheck devscripts build-essential lintian libjs-bootstrap amd64-microcode usb-modeswitch fonts-glyphicons-halflings libjs-bootstrap
+sudo apt install aptitude synaptic git curl wget locate nfs-common cups cups-common locales-all apt-xapian-index network-manager network-manager-config-connectivity-debian autoconf automake libtool unrar p7zip-full p7zip-rar cmake libudev-dev qt5-default zlib1g-dev libappindicator-dev libpulse-dev libquazip5-dev meson libsystemd-dev pkg-config libdbus-1-dev ninja-build net-tools shellcheck devscripts build-essential lintian libjs-bootstrap amd64-microcode usb-modeswitch fonts-glyphicons-halflings libjs-bootstrap libqt5x11extras5-dev libxcb-screensaver0-dev libxcb-ewmh-dev libxcb1-dev qttools5-dev
 
 sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y
 clear
